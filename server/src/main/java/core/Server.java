26dab4b9d4a236c814702b88aeda92ce767896ab
Nichil Strasser <n.strasser@serrala.com>
2021-11-11 22:53:40 +0100

    added spread connection

--- server/src/main/java/core/Server.java
+++ server/src/main/java/core/Server.java
@@ -1,14 +1,20 @@
 package core;
 
 import security.AlcatrazSecurityPolicy;
+import spread.SpreadConnection;
+import spread.SpreadException;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.security.Policy;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Server {
-    public static void main(String[] args) throws RemoteException {
+    public static void main(String[] args) throws RemoteException, UnknownHostException {
 
         Policy.setPolicy(new AlcatrazSecurityPolicy());
 
@@ -17,6 +23,28 @@ public class Server {
         }
 
         Registry registry = LocateRegistry.createRegistry(9876);
+
+        /*
+        * this list defines the host to ip mapping where the spread daemon is running
+        * */
+        HashMap<String, String> hosts = new HashMap<>();
+        hosts.put("dsiars01", "10.2.1.4");
+        hosts.put("dsiars02", "10.2.2.4");
+        hosts.put("dsiars03", "10.2.3.4");
+        hosts.put("MBP-von-Nichil", "192.168.178.52");
+
+        String hostname = InetAddress.getLocalHost().getHostName();
+
+        SpreadConnection spreadConnection = new SpreadConnection();
+        try {
+            spreadConnection.connect(InetAddress.getByName(hosts.get(hostname)), 4803, hostname, false, true);
+        } catch (SpreadException e) {
+            //TODO do something here
+            e.printStackTrace();
+        }
+
+        System.out.println(spreadConnection.getPrivateGroup());
+        System.out.println("Spread connection initialised with local daemon");
 
         System.out.println("Init done. Starting server loop");
         while (true) {
