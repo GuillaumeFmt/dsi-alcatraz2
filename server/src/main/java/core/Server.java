dcbae6bb2dab32e8459954e6eb1ace11c2f003c4
Christian H <vodoo.http@gmail.com>
2021-11-12 20:49:35 +0100

    Added basic spread send and AdvancedMessageListener logic.

--- server/src/main/java/core/Server.java
+++ server/src/main/java/core/Server.java
@@ -3,6 +3,8 @@ package core;
 import security.AlcatrazSecurityPolicy;
 import spread.SpreadConnection;
 import spread.SpreadException;
+import spread.SpreadGroup;
+import spread.SpreadMessage;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -10,11 +12,10 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.security.Policy;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class Server {
-    public static void main(String[] args) throws RemoteException, UnknownHostException {
+    public static void main(String[] args) throws RemoteException, UnknownHostException  {
 
         Policy.setPolicy(new AlcatrazSecurityPolicy());
 
@@ -24,20 +25,11 @@ public class Server {
 
         Registry registry = LocateRegistry.createRegistry(9876);
 
-        /*
-        * this list defines the host to ip mapping where the spread daemon is running
-        * */
-        HashMap<String, String> hosts = new HashMap<>();
-        hosts.put("dsiars01", "10.2.1.4");
-        hosts.put("dsiars02", "10.2.2.4");
-        hosts.put("dsiars03", "10.2.3.4");
-        hosts.put("MBP-von-Nichil", "192.168.178.52");
-
         String hostname = InetAddress.getLocalHost().getHostName();
 
         SpreadConnection spreadConnection = new SpreadConnection();
         try {
-            spreadConnection.connect(InetAddress.getByName(hosts.get(hostname)), 4803, hostname, false, true);
+            spreadConnection.connect(InetAddress.getLocalHost(), 4803, hostname, false, true);
         } catch (SpreadException e) {
             //TODO do something here
             e.printStackTrace();
@@ -46,10 +38,36 @@ public class Server {
         System.out.println(spreadConnection.getPrivateGroup());
         System.out.println("Spread connection initialised with local daemon");
 
+        SpreadGroup group = new SpreadGroup();
+        try {
+            group.join(spreadConnection, "AlcatrazGroup");
+        } catch (SpreadException e) {
+            e.printStackTrace();
+        }
+
+        spreadConnection.add(new AdvancedMessageListenerAdapter());
+        String testData = "Some Test Data";
+
+        SpreadMessage message = new SpreadMessage();
+        message.setData(testData.getBytes());
+        message.addGroup("AlcatrazGroup");
+        message.setReliable();
+        try {
+            spreadConnection.multicast(message);
+        } catch (SpreadException e) {
+            e.printStackTrace();
+        }
+
+
         System.out.println("Init done. Starting server loop");
         while (true) {
-
+            // for debugging...
         }
     }
 
 }
+
+
+
+
+
