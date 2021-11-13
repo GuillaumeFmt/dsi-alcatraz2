84279cc5a4c4fa894fc634c9cbc183e05c5769ed
Nichil Strasser <n.strasser@serrala.com>
2021-11-13 13:54:46 +0100

    added printing of Membership info for regularMembership

--- server/src/main/java/core/AdvancedMessageListenerAdapter.java
+++ server/src/main/java/core/AdvancedMessageListenerAdapter.java
@@ -1,17 +1,36 @@
 package core;
 
 import spread.AdvancedMessageListener;
+import spread.MembershipInfo;
+import spread.SpreadGroup;
 import spread.SpreadMessage;
+
+import java.lang.reflect.Member;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdvancedMessageListenerAdapter implements AdvancedMessageListener {
 
+    private static Logger logger = Logger.getLogger(AdvancedMessageListenerAdapter.class.getName());
+
     @Override
     public void regularMessageReceived(SpreadMessage spreadMessage) {
-        System.out.println("Regular message: " + new String(spreadMessage.getData()));
+        info("Regular message: " + new String(spreadMessage.getData()));
     }
 
     @Override
     public void membershipMessageReceived(SpreadMessage spreadMessage) {
-        System.out.println("Membership message: " + new String(spreadMessage.getData()));
+        info("Received Membership Message");
+        MembershipInfo membershipInfo = spreadMessage.getMembershipInfo();
+        if(membershipInfo.isRegularMembership()){
+            info("Group: " + membershipInfo.getGroup().toString());
+            info("GroupID: " + membershipInfo.getGroupID().toString());
+            info("GroupMembers: ");
+            Arrays.stream(membershipInfo.getMembers()).forEach(spreadGroup -> info(spreadMessage.toString()));
+        }
+    }
+    private void info(String message){
+        logger.log(Level.INFO, message);
     }
 }
