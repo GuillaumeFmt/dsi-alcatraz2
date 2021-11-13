e7a9bea96e8b3189fae932cc187f115b3a542890
Christian H <vodoo.http@gmail.com>
2021-11-13 17:30:45 +0100

    Added console println's for debugging in AdvancedMessageListenerAdapter

--- server/src/main/java/adapters/AdvancedMessageListenerAdapter.java
+++ server/src/main/java/adapters/AdvancedMessageListenerAdapter.java
@@ -19,13 +19,29 @@ public class AdvancedMessageListenerAdapter implements AdvancedMessageListener {
 
     @Override
     public void membershipMessageReceived(SpreadMessage spreadMessage) {
-        System.out.println("Received Membership Message");
         MembershipInfo membershipInfo = spreadMessage.getMembershipInfo();
+
+        System.out.print("Received Spread Membership Message due to ");
+        if (membershipInfo.isCausedByJoin()) {
+            System.out.println("the JOIN of " + membershipInfo.getJoined());
+        } else if (membershipInfo.isCausedByDisconnect()) {
+            System.out.println("the DISCONNECT of " + membershipInfo.getDisconnected());
+        } else if (membershipInfo.isCausedByLeave()) {
+            System.out.println("LEAVE of " + membershipInfo.getLeft());
+        } else if (membershipInfo.isCausedByNetwork()) {
+            System.out.println("a NETWORK change.");
+        } else if (membershipInfo.isTransition()) {
+            System.out.println("TRANSITIONAL membership for group " + membershipInfo.getGroup());
+        } else if (membershipInfo.isSelfLeave()) {
+            System.out.println("SELF-LEAVE message for group " + membershipInfo.getGroup());
+        }
+
         if(membershipInfo.isRegularMembership()){
             System.out.println("Group: " + membershipInfo.getGroup().toString());
             System.out.println("GroupID: " + membershipInfo.getGroupID().toString());
+            System.out.println("Number of members in group: " + Arrays.stream(membershipInfo.getMembers()).count());
             System.out.println("GroupMembers: ");
-            Arrays.stream(membershipInfo.getMembers()).forEach(spreadGroup -> System.out.println(spreadGroup.toString()));
+            Arrays.stream(membershipInfo.getMembers()).forEach(spreadGroup -> System.out.println("  " + spreadGroup.toString()));
         }
     }
 }
